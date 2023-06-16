5cbcccb113e889715b1cd56389c9d2f732c14209
Samer Ali <sameali88@gmail.com>
2023-06-16 23:29:36 +0300

    finished the task

---
+++ README.md
@@ -0,0 +1,6 @@
+1- clone the project from the github
+2- run the index.py in terminal via python
+3- check the result in the new_directory folder
+
+Note:
+this my first task in python 
---
+++ index.py
@@ -0,0 +1,36 @@
+from fnmatch import filter
+from os import path
+from os.path import isdir, join
+from shutil import copytree, rmtree
+import glob
+
+src_directory = 'orginal_directory/'
+dst_directory = 'new_directory/'
+languages_list = []
+
+def include_patterns(*patterns):
+    def _ignore_patterns(path, names):
+        keep = set(name for pattern in patterns
+                            for name in filter(names, pattern))
+        ignore = set(name for name in names
+                        if name not in keep and not isdir(join(path, name)))
+        return ignore
+    return _ignore_patterns
+
+try:
+    get_file_from_each_languages = glob.glob(src_directory + '*-1.txt')
+    for file in get_file_from_each_languages:
+        language = path.basename(file).split('-', 1)[0]
+        languages_list.append(language)
+        if isdir(dst_directory + language + '/'):
+            rmtree(dst_directory + language + '/')
+    
+    for language in languages_list:
+        temp_dst_directory = dst_directory + language + '/'
+        copytree(src_directory, temp_dst_directory, "false"
+                , ignore = include_patterns(language + '-*.txt'))
+        print(language + ' files moved to the right directory')
+except:
+    print("I don't found new files in the source directory")
+
+
